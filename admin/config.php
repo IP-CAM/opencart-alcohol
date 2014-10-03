ae1a7ee4d473b52c6413fc814590854fbe9416b3
super-oo-leg <devilaz@ukr.net>
2014-10-03 12:13:02 +0300

    repared error with single dirname() in admin config file

--- admin/config.php
+++ admin/config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER',  'http://localhost/www-opencart-test/admin/');
 define('HTTPS_CATALOG', 'http://localhost/www-opencart-test/');
 
 // DIR
-define('DIR_APPLICATION',  dirname(__FILE__) . '/admin/');
-define('DIR_SYSTEM',       dirname(__FILE__) . '/system/');
-define('DIR_LANGUAGE',     dirname(__FILE__) . '/admin/language/');
-define('DIR_TEMPLATE',     dirname(__FILE__) . '/admin/view/template/');
-define('DIR_CONFIG',       dirname(__FILE__) . '/system/config/');
-define('DIR_IMAGE',        dirname(__FILE__) . '/image/');
-define('DIR_CACHE',        dirname(__FILE__) . '/system/cache/');
-define('DIR_DOWNLOAD',     dirname(__FILE__) . '/system/download/');
-define('DIR_UPLOAD',       dirname(__FILE__) . '/system/upload/');
-define('DIR_LOGS',         dirname(__FILE__) . '/system/logs/');
-define('DIR_MODIFICATION', dirname(__FILE__) . '/system/modification/');
-define('DIR_CATALOG',      dirname(__FILE__) . '/catalog/');
+define('DIR_APPLICATION',  dirname(dirname(__FILE__)) . '/admin/');
+define('DIR_SYSTEM',       dirname(dirname(__FILE__)) . '/system/');
+define('DIR_LANGUAGE',     dirname(dirname(__FILE__)) . '/admin/language/');
+define('DIR_TEMPLATE',     dirname(dirname(__FILE__)) . '/admin/view/template/');
+define('DIR_CONFIG',       dirname(dirname(__FILE__)) . '/system/config/');
+define('DIR_IMAGE',        dirname(dirname(__FILE__)) . '/image/');
+define('DIR_CACHE',        dirname(dirname(__FILE__)) . '/system/cache/');
+define('DIR_DOWNLOAD',     dirname(dirname(__FILE__)) . '/system/download/');
+define('DIR_UPLOAD',       dirname(dirname(__FILE__)) . '/system/upload/');
+define('DIR_LOGS',         dirname(dirname(__FILE__)) . '/system/logs/');
+define('DIR_MODIFICATION', dirname(dirname(__FILE__)) . '/system/modification/');
+define('DIR_CATALOG',      dirname(dirname(__FILE__)) . '/catalog/');
 
 // DB
 define('DB_DRIVER',   'mysqli');
