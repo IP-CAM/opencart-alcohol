65acffe917971f56f6dd66e8ba0a89056a619285
super-oo-leg <devilaz@ukr.net>
2014-10-03 11:48:01 +0300

    fresh opencart cms

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/www-opencart-test/admin/');
+define('HTTP_CATALOG', 'http://localhost/www-opencart-test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/www-opencart-test/admin/');
+define('HTTPS_CATALOG', 'http://localhost/www-opencart-test/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/www-opencart-test/admin/');
+define('DIR_SYSTEM', '/var/www/html/www-opencart-test/system/');
+define('DIR_LANGUAGE', '/var/www/html/www-opencart-test/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/www-opencart-test/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/www-opencart-test/system/config/');
+define('DIR_IMAGE', '/var/www/html/www-opencart-test/image/');
+define('DIR_CACHE', '/var/www/html/www-opencart-test/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/www-opencart-test/system/download/');
+define('DIR_UPLOAD', '/var/www/html/www-opencart-test/system/upload/');
+define('DIR_LOGS', '/var/www/html/www-opencart-test/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/www-opencart-test/system/modification/');
+define('DIR_CATALOG', '/var/www/html/www-opencart-test/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '200ok');
+define('DB_DATABASE', 'opencart-test');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/www-opencart-test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/www-opencart-test/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/www-opencart-test/catalog/');
+define('DIR_SYSTEM', '/var/www/html/www-opencart-test/system/');
+define('DIR_LANGUAGE', '/var/www/html/www-opencart-test/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/www-opencart-test/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/www-opencart-test/system/config/');
+define('DIR_IMAGE', '/var/www/html/www-opencart-test/image/');
+define('DIR_CACHE', '/var/www/html/www-opencart-test/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/www-opencart-test/system/download/');
+define('DIR_UPLOAD', '/var/www/html/www-opencart-test/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/www-opencart-test/system/modification/');
+define('DIR_LOGS', '/var/www/html/www-opencart-test/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '200ok');
+define('DB_DATABASE', 'opencart-test');
+define('DB_PREFIX', 'oc_');
